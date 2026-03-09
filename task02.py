7e912f1dc55c3866c2bc5591f8d89f0ea52975bf
dvankevich <dvankevich@gmail.com>
2026-03-09 14:41:44 +0200

    add Book class

--- task02.py
+++ task02.py
@@ -1,46 +1,39 @@
-class Library:
-    def __init__(self):
-        self.books = []
+from abc import ABC, abstractmethod
 
-    def add_book(self, title, author, year):
-        book = {
-            "title": title,
-            "author": author,
-            "year": year
-        }
-        self.books.append(book)
+class Book:
+    pass
 
-    def remove_book(self, title):
-        for book in self.books:
-            if book["title"] == title:
-                self.books.remove(book)
-                break
+class LibraryInterface(ABC):
+    pass
 
-    def show_books(self):
-        for book in self.books:
-            print(f'Title: {book["title"]}, Author: {book["author"]}, Year: {book["year"]}')
+class Library(LibraryInterface):
+    pass
+
+class LibraryManager:
+    pass
 
 def main():
     library = Library()
-    
+    manager = LibraryManager(library)
+
     while True:
         command = input("Enter command (add, remove, show, exit): ").strip().lower()
-        
-        if command == "add":
-            title = input("Enter book title: ").strip()
-            author = input("Enter book author: ").strip()
-            year = input("Enter book year: ").strip()
-            library.add_book(title, author, year)
-        elif command == "remove":
-            title = input("Enter book title to remove: ").strip()
-            library.remove_book(title)
-        elif command == "show":
-            library.show_books()
-        elif command == "exit":
-            break
-        else:
-            print("Invalid command. Please try again.")
+
+        match command:
+            case "add":
+                title = input("Enter book title: ").strip()
+                author = input("Enter book author: ").strip()
+                year = input("Enter book year: ").strip()
+                manager.add_book(title, author, year)
+            case "remove":
+                title = input("Enter book title to remove: ").strip()
+                manager.remove_book(title)
+            case "show":
+                manager.show_books()
+            case "exit":
+                break
+            case _:
+                print("Invalid command. Please try again.")
 
 if __name__ == "__main__":
     main()
-
